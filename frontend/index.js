0afbf9985ced9c9d358abb7fdd5dd1ea8e011e02
RasmusMeinche <rame0001@stud.kea.dk>
2026-08-24 00:25:50 +0200

    Desert World + PlayerSpriteDown added. Centered player with cropping + coordinates and started to implement a walking funktion

--- frontend/index.js
+++ frontend/index.js
@@ -6,8 +6,27 @@ canvas.height = window.innerHeight
 let c = canvas.getContext("2d")
 
 const desertMap = new Image()
-desertMap.src = "../images/map.png"
+desertMap.src = "./images/worlds/DesertTileset.png"
+
+const playerDown = new Image()
+playerDown.src = "./images/playerMovement/PlayerSpriteDown(1).png"
 
 desertMap.onload = () => {
     c.drawImage(desertMap, 0, 0)
+    c.drawImage(
+        playerDown,
+        0,
+        0,
+        playerDown.width / 6 + 2,
+        playerDown.height,
+        canvas.width / 2 - (playerDown.width / 6) / 2,
+        canvas.height / 2 - playerDown.height / 2,
+        playerDown.width / 6,
+        playerDown.height
+    )
+
+    window.addEventListener("keydown", () => {
+        console.log("Du har trykket")
+    })
+        
 }
