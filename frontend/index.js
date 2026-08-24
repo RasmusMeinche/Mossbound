11450066b4c679edc15a154a9a24541843ef83ac
RasmusMeinche <rame0001@stud.kea.dk>
2026-08-24 11:24:16 +0200

    Spiller-bevægelse på alle akser

--- frontend/index.js
+++ frontend/index.js
@@ -9,10 +9,49 @@ const desertMap = new Image()
 desertMap.src = "./images/worlds/DesertTileset.png"
 
 const playerDown = new Image()
-playerDown.src = "./images/playerMovement/PlayerSpriteDown(1).png"
+playerDown.src = "./images/playerMovement/PlayerSpriteDown.png"
 
-desertMap.onload = () => {
-    c.drawImage(desertMap, 0, 0)
+class Sprite {
+    constructor({
+        position,
+        image
+    }) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+
+}
+
+const background = new Sprite({
+    position: {
+    x: -2000,
+    y: -2000
+    },
+    image: desertMap
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animate () {
+    window.requestAnimationFrame(animate)
+    background.draw()
     c.drawImage(
         playerDown,
         0,
@@ -25,8 +64,49 @@ desertMap.onload = () => {
         playerDown.height
     )
 
-    window.addEventListener("keydown", () => {
-        console.log("Du har trykket")
-    })
-        
+    if (keys.w.pressed && keys.a.pressed) background.position.x = background.position.x + 2, background.position.y = background.position.y + 2
+    else if (keys.w.pressed && keys.d.pressed) background.position.x = background.position.x - 2, background.position.y = background.position.y + 2
+    else if (keys.s.pressed && keys.a.pressed) background.position.x = background.position.x + 2, background.position.y = background.position.y - 2
+    else if (keys.s.pressed && keys.d.pressed) background.position.x = background.position.x - 2, background.position.y = background.position.y - 2
+    else if (keys.w.pressed) background.position.y = background.position.y + 3
+    else if (keys.a.pressed) background.position.x = background.position.x + 3
+    else if (keys.s.pressed) background.position.y = background.position.y - 3
+    else if (keys.d.pressed) background.position.x = background.position.x - 3
 }
+animate()
+
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "w":
+            keys.w.pressed = true
+        break
+        case "a":
+            keys.a.pressed = true
+        break
+        case "s":
+            keys.s.pressed = true
+        break
+        case "d":
+            keys.d.pressed = true
+        break
+    }
+    console.log(keys)
+})
+
+window.addEventListener("keyup", (e) => {
+    switch (e.key) {
+        case "w":
+            keys.w.pressed = false
+        break
+        case "a":
+            keys.a.pressed = false
+        break
+        case "s":
+            keys.s.pressed = false
+        break
+        case "d":
+            keys.d.pressed = false
+        break
+    }
+})
+        
